f53c90a8e3f594c0787dba00c67f0867b006f059
Amara <amar_algu@hotmail.com>
2021-10-03 15:55:09 -0400

    polish layout, display set as wanted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,25 @@
 let searchEL = document.querySelector("#search");
 let results = document.querySelector("#weather");
-let day1 =document.querySelector("#day1");
-let day2 =document.querySelector("#day2");
-let day3 =document.querySelector("#day3");
-let day4 =document.querySelector("#day4");
-let day5 =document.querySelector("#day5");
+let day1 = document.querySelector("#day1");
+let day2 = document.querySelector("#day2");
+let day3 = document.querySelector("#day3");
+let day4 = document.querySelector("#day4");
+let day5 = document.querySelector("#day5");
 let form = document.querySelector("#form");
 let apiKey = "&appid=2cd9d91e9448c1dfc0ccd7c1ee798680"
 let apiKeyDays = "&cnt=5&appid=2cd9d91e9448c1dfc0ccd7c1ee798680"
 let apiBaseURL = "https://api.openweathermap.org/data/2.5/weather?q="
 let apiFuture = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&cnt=5"
-
+let searchHistory = [];
+let historyContainer = document.getElementById("historyContainer");
 
 
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'))
-$("#day1").text(moment().add(1, 'day').format('dddd MMMM Do YYYY'))
-$("#day2").text(moment().add(2, 'days').format('dddd MMMM Do YYYY'))
-$("#day3").text(moment().add(3, 'days').format('dddd MMMM Do YYYY'))
-$("#day4").text(moment().add(4, 'days').format('dddd MMMM Do YYYY'))
-$("#day5").text(moment().add(5, 'days').format('dddd MMMM Do YYYY'))
-// var a = moment().toString();
-//          document.getElementById("todaysdate").innerHTML = a;
+
+
+
+
+
 
 console.log("I am here")
 
@@ -30,7 +29,8 @@ function conductSearch(event) {
     event.preventDefault();
     let search = document.querySelector("#input").value;
     let apiCurrentWeather = apiBaseURL + search + apiKey;
-   
+
+    setLocalStorage(search)
 
     console.log(search);
 
@@ -38,6 +38,27 @@ function conductSearch(event) {
     fetchApi(apiCurrentWeather)
 }
 console.log("I am here")
+function setLocalStorage(search) {
+    searchHistory.push(search)
+
+    localStorage.setItem("searchHistory", searchHistory);
+    renderSearchHistory()
+}
+
+function renderSearchHistory() {
+    localStorage.getItem("searchHistory")
+
+    for (var i = 0; i < searchHistory.length; i++) {
+        //let listItem = document.createElement("li")
+        //let btn = document.createElement("button");
+        //btn.setAttribute("type", "button")
+       // btn.setAttribute("class", "historyBtn")
+       // btn.setAttribute('data-search', searchHistory[i])
+       // btn.textContent = searchHistory[i];
+        //listItem.appendChild(btn)
+        //historyContainer.appendChild(listItem);
+    }
+}
 
 function fetchApi(apiCurrentWeather) {
     fetch(apiCurrentWeather)
@@ -48,86 +69,120 @@ function fetchApi(apiCurrentWeather) {
         })
         .then(function (data) {
 
-                
+
             console.log(data);
             results.innerHTML = ""
-           
+
             results.innerHTML += `<h3> ${data.name}</h3>`;
-            
-
-           // document.body.querySelector("#weather").innerHTML 
-
-        
-        
-        let apiFutureDays = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&cnt=5&appid=2cd9d91e9448c1dfc0ccd7c1ee798680`   
-        
-        fetch(apiFutureDays)
-             console.log(apiFutureDays)
-             fetch(apiFutureDays)
-             .then(function (response) {
-                 return response.json();
-     
-                 })
-             .then(function (data1) {
-                     
-                 console.log(data1);
-                 results.innerHTML += `<p>Temp  ${parseInt(data1.current.temp-273)} °C </p>`;
-                 results.innerHTML += `<p>Pressure ${data1.current.pressure} hPa </p>`;
-                 results.innerHTML += `<p>Visibility ${data1.current.visibility/1000} km</p>`;
-                 results.innerHTML += `<p>Wind speed ${data1.current.wind_speed} MPH</p>`;
-                 results.innerHTML += `<p>Humitdity ${data1.current.humidity} % </p>`;
-                 results.innerHTML += `<p> UV Index ${data1.current.uvi}</p>`;
-                 document.body.querySelector("#weather")
-                
-                
-                 
-                 document.querySelector("#day1")
-                 console.log(data1);
-                 day1.innerHTML += `<p>${data1.daily[1].weather[0].icon}</p>`;
-                 day1.innerHTML += `<p>Temp  ${parseInt(data1.daily[1].temp.day-273)} °C </p>`;
-                 day1.innerHTML += `<p>Wind Speed  ${data1.daily[1].wind_speed} MPH </p>`;
-                 day1.innerHTML += `<p>Humidity  ${data1.daily[1].humidity} % </p>`;
-                
-                
-                 document.querySelector("#day2")
-                 console.log(data1);
-                 day2.innerHTML += `<p>Temp  ${parseInt(data1.daily[2].temp.day-273)} °C </p>`;
-                 day2.innerHTML += `<p>Wind Speed  ${data1.daily[2].wind_speed} MPH </p>`;
-                 day2.innerHTML += `<p>Humidity  ${data1.daily[2].humidity} % </p>`;
-
-                 document.querySelector("#day3")
-                 console.log(data1);
-                 day3.innerHTML += `<p>Temp  ${parseInt(data1.daily[3].temp.day-273)} °C </p>`;
-                 day3.innerHTML += `<p>Wind Speed  ${data1.daily[3].wind_speed} MPH </p>`;
-                 day3.innerHTML += `<p>Humidity  ${data1.daily[3].humidity} % </p>`;
-
-                 document.querySelector("#day4")
-                 console.log(data1);
-                 day4.innerHTML += `<p>Temp  ${parseInt(data1.daily[4].temp.day-273)} °C </p>`;
-                 day4.innerHTML += `<p>Wind Speed  ${data1.daily[4].wind_speed} MPH </p>`;
-                 day4.innerHTML += `<p>Humidity  ${data1.daily[4].humidity} % </p>`;
-
-                 document.querySelector("#day5")
-                 console.log(data1);
-                 day5.innerHTML += `<p>Temp  ${parseInt(data1.daily[5].temp.day-273)} °C </p>`;
-                 day5.innerHTML += `<p>Wind Speed  ${data1.daily[5].wind_speed} MPH </p>`;
-                 day5.innerHTML += `<p>Humidity  ${data1.daily[5].humidity} % </p>`;
 
 
-             });
-             
-                 
-                 
-                 
+            document.body.querySelector("#weather").innerHTML
+
+
+
+            let apiFutureDays = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&cnt=5&appid=2cd9d91e9448c1dfc0ccd7c1ee798680`
+
+            fetch(apiFutureDays)
+            console.log(apiFutureDays)
+            fetch(apiFutureDays)
+                .then(function (response) {
+                    return response.json();
+
+                })
+                .then(function (data1) {
+                    results.innerHTML = "";
+                    results.innerHTML += `<h3> ${data.name}</h3>`;
+
+
+                    document.body.querySelector("#weather").innerHTML
+
+                    let uviBadge = document.createElement("button")
+                    let uviElement = document.createElement('p')
+                    console.log(data1);
+
+                    results.innerHTML += `<p>Temp  ${parseInt(data1.current.temp - 273)} °C </p>`;
+                    results.innerHTML += `<p>Pressure ${data1.current.pressure} hPa </p>`;
+                    results.innerHTML += `<p>Visibility ${data1.current.visibility / 1000} km</p>`;
+                    results.innerHTML += `<p>Wind speed ${data1.current.wind_speed} MPH</p>`;
+                    results.innerHTML += `<p>Humitdity ${data1.current.humidity} % </p>`;
+                    uviElement.textContent = `UV Index ${data1.current.uvi}`;
+                    uviBadge.append(uviElement)
+                    console.log("uvi badge", uviBadge)
+                    results.append(uviBadge);
+
+                    if (data1.current.uvi < 2) {
+                        uviBadge.setAttribute('class', "btn-success btn btn-sm")
+                    } else if (data1.current.uvi < 5) {
+                        uviBadge.setAttribute('class', 'btn-warning btn btn-sm')
+                    } else {
+                        uviBadge.setAttribute("class", 'btn-danger btn btn-sm')
+                    }
+                    document.body.querySelector("#weather")
+
+
+
+                    document.querySelector("#day1")
+                    console.log(data1);
+                    day1.innerHTML = "";
+                    $("#day1").text(moment().add(1, 'days').format('DD/MM/YYYY'))
+                    day1.innerHTML += `<img src= https://openweathermap.org/img/w/${data1.daily[1].weather[0].icon}.png></img>`;
+                    day1.innerHTML += `<p>Temp  ${parseInt(data1.daily[1].temp.day - 273)} °C </p>`;
+                    day1.innerHTML += `<p>Wind Speed  ${data1.daily[1].wind_speed} MPH </p>`;
+                    day1.innerHTML += `<p>Humidity  ${data1.daily[1].humidity} % </p>`;
+
+
+                    document.querySelector("#day2")
+                    console.log(data1);
+                    day2.innerHTML = "";
+                    $("#day2").text(moment().add(2, 'days').format('DD/MM/YYYY'))
+                    day2.innerHTML += `<img src= https://openweathermap.org/img/w/${data1.daily[2].weather[0].icon}.png></img>`;
+                    day2.innerHTML += `<p>Temp  ${parseInt(data1.daily[2].temp.day - 273)} °C </p>`;
+                    day2.innerHTML += `<p>Wind Speed  ${data1.daily[2].wind_speed} MPH </p>`;
+                    day2.innerHTML += `<p>Humidity  ${data1.daily[2].humidity} % </p>`;
+
+                    document.querySelector("#day3")
+                    console.log(data1);
+                    day3.innerHTML = "";
+                    $("#day3").text(moment().add(3, 'days').format('DD/MM/YYYY'))
+                    day3.innerHTML += `<img src= https://openweathermap.org/img/w/${data1.daily[3].weather[0].icon}.png></img>`;
+                    day3.innerHTML += `<p>Temp  ${parseInt(data1.daily[3].temp.day - 273)} °C </p>`;
+                    day3.innerHTML += `<p>Wind Speed  ${data1.daily[3].wind_speed} MPH </p>`;
+                    day3.innerHTML += `<p>Humidity  ${data1.daily[3].humidity} % </p>`;
+
+                    document.querySelector("#day4")
+                    day4.innerHTML = "";
+                    console.log(data1);
+                    $("#day4").text(moment().add(4, 'days').format('DD/MM/YYYY'))
+                    day4.innerHTML += `<img src= https://openweathermap.org/img/w/${data1.daily[4].weather[0].icon}.png></img>`;
+                    day4.innerHTML += `<p>Temp  ${parseInt(data1.daily[4].temp.day - 273)} °C </p>`;
+                    day4.innerHTML += `<p>Wind Speed  ${data1.daily[4].wind_speed} MPH </p>`;
+                    day4.innerHTML += `<p>Humidity  ${data1.daily[4].humidity} % </p>`;
+
+                    document.querySelector("#day5")
+                    day5.innerHTML = "";
+                    console.log(data1);
+                    $("#day5").text(moment().add(5, 'days').format('DD/MM/YYYY'))
+                    day5.innerHTML += `<img src= https://openweathermap.org/img/w/${data1.daily[5].weather[0].icon}.png></img>`;
+                    day5.innerHTML += `<p>Temp  ${parseInt(data1.daily[5].temp.day - 273)} °C </p>`;
+                    day5.innerHTML += `<p>Wind Speed  ${data1.daily[5].wind_speed} MPH </p>`;
+                    day5.innerHTML += `<p>Humidity  ${data1.daily[5].humidity} % </p>`;
+
+                    let forecastDiv = document.getElementById("forecast")
+                    forecastDiv.append(day1, day2, day3, day4, day5)
+                });
+
+
+
+
         });
-    }
-        
+}
 
-        
 
-    
 
-    
-    
-   
+
+
+
+
+
+
 
