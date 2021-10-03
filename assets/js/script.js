1e39ee7d10aabfb8bc36e42e638c0c2f8698c4fc
Amara <amar_algu@hotmail.com>
2021-10-03 04:47:23 -0400

    script file updated with daily forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
-
 let searchEL = document.querySelector("#search");
 let results = document.querySelector("#weather");
 let form = document.querySelector("#form");
 let apiKey = "&appid=2cd9d91e9448c1dfc0ccd7c1ee798680"
+let apiKeyDays = "&cnt=5&appid=2cd9d91e9448c1dfc0ccd7c1ee798680"
 let apiBaseURL = "https://api.openweathermap.org/data/2.5/weather?q="
-let apiFuture = "https://api.openweathermap.org/data/2.5/forecast/daily?"
-let fiveDays = "&cnt=5"
-var a = moment().toString();
-         document.getElementById("todaysdate").innerHTML = a;
+let apiFuture = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&cnt=5"
+
+
+
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'))
+// var a = moment().toString();
+//          document.getElementById("todaysdate").innerHTML = a;
+
 console.log("I am here")
 
 form.addEventListener("submit", conductSearch);
@@ -15,72 +19,71 @@ form.addEventListener("submit", conductSearch);
 function conductSearch(event) {
     event.preventDefault();
     let search = document.querySelector("#input").value;
-    let apiCurrentWether = apiBaseURL + search + apiKey;
+    let apiCurrentWeather = apiBaseURL + search + apiKey;
+   
 
     console.log(search);
 
 
-    fetchApi(apiCurrentWether)
+    fetchApi(apiCurrentWeather)
 }
+console.log("I am here")
 
-function fetchApi(apiCurrentWether) {
-    fetch(apiCurrentWether)
+function fetchApi(apiCurrentWeather) {
+    fetch(apiCurrentWeather)
         .then(function (response) {
             return response.json();
 
 
         })
         .then(function (data) {
+
+                
             console.log(data);
             results.innerHTML = ""
            
             results.innerHTML += `<h3> ${data.name}</h3>`;
-            results.innerHTML += `<p>Temp  ${parseInt(data.main.temp-273)} °C </p>`;
-            results.innerHTML += `<p>Pressure ${data.main.pressure} hPa </p>`;
-            results.innerHTML += `<p>Visibility ${data.visibility/1000} km</p>`;
-            results.innerHTML += `<p>Wind speed ${data.wind.speed} MPH</p>`;
-            results.innerHTML += `<p>Humitdity ${data.main.humidity} % </p>`;
+            
 
-            //results.innerHTML = `${data}`;
-            //for (let i = 0; i < data.results.lenght; i++) {
-                
+           // document.body.querySelector("#weather").innerHTML 
+
         
-            //console.log(JSON.stringify(results));
-            document.body.querySelector("#weather").innerHTML 
-
-
-
+        console.log(data.coord.lat);
+        console.log(data.coord.lon);
+        let apiFutureDays = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&cnt=5&appid=2cd9d91e9448c1dfc0ccd7c1ee798680`   
+        
+        fetch(apiFutureDays)
+             console.log(apiFutureDays)
+             fetch(apiFutureDays)
+             .then(function (response) {
+                 return response.json();
+     
+                 })
+             .then(function (data1) {
+                     
+                 console.log(data1);
+                 results.innerHTML += `<p>Temp  ${parseInt(data1.current.temp-273)} °C </p>`;
+                 results.innerHTML += `<p>Pressure ${data1.current.pressure} hPa </p>`;
+                 results.innerHTML += `<p>Visibility ${data1.current.visibility/1000} km</p>`;
+                 results.innerHTML += `<p>Wind speed ${data1.current.weather.wind_speed} MPH</p>`;
+                 results.innerHTML += `<p>Humitdity ${data1.current.humidity} % </p>`;
+                 results.innerHTML += `<p> UV Index ${data1.current.uvi}</p>`;
+                 document.body.querySelector("weather")
+             });
+             
+                 document.querySelector("day1")
+                 console.log(data1);
+                 
+                 
         });
-
-        //function displayResults(data){
-
-   
-    //= `<p> ${data} <p>`,
-     //   `<p>${data.main.temp}<p>`
-
     }
-    // GIVEN a weather dashboard with form inputs
-    // WHEN I search for a city
-    // THEN I am presented with current and future conditions for that city and that city is added to the search history
-    // WHEN I view current weather conditions for that city
-    // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-    // WHEN I view the UV index
-    // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-    // WHEN I view future weather conditions for that city
-    // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-    // WHEN I click on a city in the search history
-    // THEN I am again presented with current and future conditions for that city
         
 
+        
 
+    
 
-
-
-
-
-
-
-
-
-
+    
+    
+   
 
